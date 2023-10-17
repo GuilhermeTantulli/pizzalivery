766964b9c406111d03c6a6aa713cf147cb7599c0
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2023-10-17 12:30:16 -0300

    Updated: Inseridas as querys para consulta de API

--- src/pages/sizes/Sizes.tsx
+++ src/pages/sizes/Sizes.tsx
@@ -11,53 +11,22 @@ export default function Sizes() {
   const navigate = useNavigate()
   const { pizzaSize, setPizzaSize } = useContext(OrderContext)
 
-
-  const sizeOptions = [
-    {
-      id: "10",
-      flavours: 1,
-      size: 35,
-      slices: 8,
-      text: "Grande",
-    },
-    {
-      id: "11",
-      flavours: 2,
-      size: 35,
-      slices: 8,
-      text: "Grande",
-    },
-    {
-      id: "20",
-      flavours: 1,
-      size: 28,
-      slices: 4,
-      text: "Média",
-    },
-    {
-      id: "21",
-      flavours: 2,
-      size: 28,
-      slices: 4,
-      text: "Média",
-    },
-    {
-      id: "30",
-      flavours: 1,
-      size: 18,
-      slices: 1,
-      text: "Broto",
-    },
-    {
-      id: "31",
-      flavours: 2,
-      size: 18,
-      slices: 1,
-      text: "Broto",
-    },
-  ]
-
   const [sizeId, setSizeId] = useState('')
+  const [pizzaSizeOptions, setPizzaSizeOptions] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getPizzaSizeOptions = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch("http://localhost:8000/pizza/sizes")
+      const options = await response.json()
+      setPizzaSizeOptions(options)
+    } catch (error) {
+      alert(`Deu ruim: ${error}`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   const getPizzaSize = (sizeId: string) => {
     return sizeOptions.filter((option) => option.id === sizeId)
@@ -91,17 +60,15 @@ export default function Sizes() {
   return (
     <Layout>
       <Title tabIndex={0}>Escolha o tamanho da sua pizza</Title>
-      <SizeContentWrapper>
-        {sizeOptions.map(({ id, size, slices, flavours, text }) => (
+      {isLoading ? (
+        <SizeContentWrapper>
+          <Title>Carregando...</Title>
+        </SizeContentWrapper>
+        ) : (
+        <SizeContentWrapper>
+        {pizzaSizeOptions.map(({id, size, slices, flavours, text}) => (
           <RadioCard key={id}>
-            <input
-              type="radio"
-              id={id}
-              name="sizes"
-              onChange={handleChange}
-              value={id}
-              checked={sizeId === id}
-            />
+            <input type="radio" id={id} name="sizes" onChange={handleChange} value={id} checked={sizeId === id}/>
             <label htmlFor={id}>
               {text} - {flavours} sabores
               <span>
@@ -110,12 +77,11 @@ export default function Sizes() {
             </label>
           </RadioCard>
         ))}
-      </SizeContentWrapper>
+        </SizeContentWrapper>
+      )}
       <SizeActionWrapper>
-        <Button inverse="inverse" onClick={handleBack}>
-          Voltar
-        </Button>
-        <Button onClick={handleNext}>Escolha o sabor</Button>
+        <Button inverse="inverse" onClick={handleBack}>Voltar</Button>
+        <Button onClick={handleNext}>Escolha o Sabor</Button>
       </SizeActionWrapper>
     </Layout>
   )
